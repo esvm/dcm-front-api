970ae5a4b1a204cd1712285eb3d30298ca8c7bfa
Luiz Reis <luiz.nogueira@inlocomedia.com>
2019-07-02 03:15:40 -0300

    fix(index.js, api.proto): fix connection with grpc server

--- index.js
+++ index.js
@@ -3,42 +3,50 @@ const grpc = require("grpc");
 
 const apiPackage = require("./proto.js").api;
 
-const grpcHost = process.env.GRPC_HOST || "localhost";
+const grpcHost = process.env.GRPC_HOST || "104.196.58.169";
 const grpcPort = process.env.GRPC_PORT || 50051;
-const topic = process.env.TOPIC_NAME || "iot";
+const topic = process.env.TOPIC_NAME || "disk_used";
 
-const api = apiPackage.DcmApiDatabaseService(
+const api = new apiPackage.DcmApiDatabaseService(
   `${grpcHost}:${grpcPort}`,
   grpc.credentials.createInsecure()
 );
 
 const server = http.createServer((request, response) => {
   console.log("Requested url: " + request.url);
+  switch (request.url.toLowerCase()) {
+    case "/events":
+      response.writeHead(200, {
+        Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*"
+      });
 
-  if (request.url.toLowerCase() === "/events") {
-    response.writeHead(200, {
-      "Connection": "keep-alive",
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "*"
-    });
-
-    setTimeout(() => {
       const payload = {
         topic_name: topic,
-        minutes_ago: 5
+        minutes_ago: 2
       };
-      let items = api.GetItems(payload);
-      const response = {
-        title: "Average",
-        array: items
-      };
-      response.write(`data: ${JSON.stringify(response)}`);
-      response.write("\n\n");
-    }, 300);
-  } else {
-    response.writeHead(404);
-    response.end();
+
+      api.GetItems(payload, (err, items) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const res = {
+          title: "metrics",
+          ...items
+        };
+
+        response.write(`data: ${JSON.stringify(res)}`);
+        response.write("\n\n");
+        response.end();
+      });
+      break;
+    default:
+      response.writeHead(404);
+      response.end();
   }
 });
 
